3b1a473a2586dff678abbb1bdcd58c16f95ca5d7
RedstoneDaedalus <redstonedaedalus@gmail.com>
2018-01-06 20:13:32 -0700

    Changed clientOptions passed.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Client = require("./base/Client");
 
-const client = new Client({ disableEveryone: true, disabledEvents: ["USER_UPDATE", "TYPING_START"] })
+const client = new Client({ fetchAllMembers: true, disableEveryone: true, disabledEvents: ["USER_UPDATE", "TYPING_START"] })
     .setConfig("./config.json")
     .loadCommands(`./commands`)
     .loadEvents(`./events`);
