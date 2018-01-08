43d522b8df3ab13e5ed63ead833a32c19219597f
RedstoneDaedalus <redstonedaedalus@gmail.com>
2018-01-07 21:11:26 -0700

    Added storage of timestamp.

--- commands/system/restart.js
+++ commands/system/restart.js
@@ -17,7 +17,7 @@ module.exports = class Restart extends Base {
         // Send a message notifying the user of a restart
         const msg = await message.channel.send(`<a:typing:398270961163436044> | Restarting...`);
         // Write information to restart.json
-        writeFile("restart.json", `{ "id": "${msg.id}", "channel": "${message.channel.id}" }`, () => {
+        writeFile("restart.json", `{ "id": "${msg.id}", "channel": "${message.channel.id}", "time": "${Date.now()}" }`, () => {
             // When write is complete, end process
             process.exit(1);
         });
