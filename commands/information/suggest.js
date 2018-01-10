b67b135629288e5197000fb93836804313bb6984
RedstoneDaedalus <redstonedaedalus@gmail.com>
2018-01-09 20:02:10 -0700

    Created suggest command that redirects the user to the forums.

---
+++ commands/information/suggest.js
@@ -0,0 +1,17 @@
+const Base = require("../../base/Command.js");
+
+module.exports = class Suggest extends Base {
+    constructor(client) {
+        super(client, {
+            name: "suggest",
+            description: "Please use the forums. (http://www.mcdiamondfire.com/forum)",
+            usage: "",
+            category: "information",
+            permLevel: 0 
+        });
+    }
+
+    run(message) {
+        super.respond(`Post your suggestions on the forums. (http://www.mcdiamondfire.com/forum)`);
+    }
+};
