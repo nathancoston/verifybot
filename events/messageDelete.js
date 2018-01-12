d9e6e915146d09204d5d13c3f3b19cc80f02acf3
RedstoneDaedalus <redstonedaedalus@gmail.com>
2018-01-11 20:20:57 -0700

    Added additional checks and changed color.

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -7,18 +7,24 @@ module.exports = class {
     }
 
     async run(message) {
+        // If message was not sent in guild, return
+        if (message.guild.id !== config.guild) return;
         // Fetch logs channel
-        const logs = this.client.channels.find("name", config.logs.deleted);
+        const logs = this.client.channels.find("name", config.logs.messageUpdates);
         // If no logs channel found, return false
         if (!logs) return false;
+        // If message is a command or message is sent by a bot, ignore it
+        if ((message.content.startsWith(config.prefix) && this.client.commands.has(message.content.split(config.prefix)[1])) || message.author.bot) return;
 
         // Create a new embed
         const embed = new MessageEmbed()
-            .setColor(0xFF0000)
-            .setAuthor(message.author.displayName)
-            .setTitle(`Deleted message in ${message.channel}.`)
+            .setColor([255, 100, 100])
+            .setAuthor(`${message.member.displayName} (${message.author.tag})`)
+            .setTitle(`Deleted message in #${message.channel.name}.`)
             .setDescription(message.content)
-            .setImage(message.attachments.size > 0 ? message.attachments.first().url : null);
+            .setImage(message.attachments.size > 0 ? message.attachments.first().url : null)
+            .setFooter("Deleted")
+            .setTimestamp(new Date());
 
         // Send the created embed
         logs.send(embed);
