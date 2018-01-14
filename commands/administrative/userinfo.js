6b6fb6b47e4e8c88cf2af4a4061adf3f51213aa5
RedstoneDaedalus <redstonedaedalus@gmail.com>
2018-01-13 17:32:34 -0700

    Fixed spacing. Added comments. Fixed permission level field.

--- commands/administrative/userinfo.js
+++ commands/administrative/userinfo.js
@@ -3,45 +3,49 @@ const Base = require("../../base/Command.js");
 
 module.exports = class Userinfo extends Base {
   constructor(client) {
-    super(client, {
-      name: "userinfo",
-      description: "Finds information on a user.",
-      usage: "<user>",
-      category: "administrative",
-      permLevel: 5
-    });
+      super(client, {
+          name: "userinfo",
+          description: "Finds information on a user.",
+          usage: "<user>",
+          category: "administrative",
+          permLevel: 5
+      });
   }
-  
+
   async run(message, args) {
-    // Fetch the target user
-    const user = await super.verifyUser(args[0]);
-    const member = await message.guild.members.fetch(user.id);
-    const permLevel = this.client.permLevel(user.id);
-    // Confirm that a user has been found
-    if (!user) return super.error("Unknown user.");
-    
-    // Create a new embed
-    const embed = new MessageEmbed()
-      .setColor("#FFFFFF")
-      .setAuthor(user.tag)
-      .setFooter("VerifyBot User Information");
-     
-    // Add fields
-   embed.addField("» Name", user.username, true);
-   if (member.displayName !== user.username) embed.addField("» Nickname", member.displayName, true);
-   embed.addField("» Roles", member.roles.filter(r => r.id !== member.guild.id).map(r => r.name).join(", "), true);
-   embed.addField("» Permission Level", `**${permLevel.level}** (\`${permLevel.name}\`)`);
-   embed.addField("» Joined Discord", this.humanize(user.createdAt), true);
-   embed.addField("» Joined Server", this.humanize(member.joinedAt), true);
-    
-   message.channel.send({ embed });
+      // Fetch the target user
+      const user = await super.verifyUser(args[0]);
+      // Fetch the guild member
+      const member = await message.guild.members.fetch(user.id);
+      // Fetch the user's permission level
+      const permLevel = await this.client.permLevel(user.id);
+      // Confirm that a user has been found
+      if (!user) return super.error("Unknown user.");
+
+      // Create a new embed
+      const embed = new MessageEmbed()
+          .setColor("#FFFFFF")
+          .setAuthor(user.tag)
+          .setFooter("VerifyBot User Information");
+
+      // Add fields
+      embed.addField("» Name", user.username, true);
+      if (member.displayName !== user.username) embed.addField("» Nickname", member.displayName, true);
+      embed.addField("» Roles", member.roles.filter(r => r.id !== member.guild.id).map(r => r.name).join(", "), true);
+      embed.addField("» Permission Level", `**${permLevel.level}** (\`${permLevel.name}\`)`, true);
+      embed.addField("» Joined Discord", this.humanize(user.createdAt), true);
+      embed.addField("» Joined Server", this.humanize(member.joinedAt), true);
+
+      // Send the embed
+      message.channel.send({
+          embed
+      });
   }
-  
+
   // Used to make dates easy to read
   humanize(date) {
-    // Define months
-    const months = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    return `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
+      // Define months
+      const months = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+      return `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
   }
 };
-
