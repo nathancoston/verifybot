1d788761263ea14d19b1709bb05b58d153cb3508
RedstoneDaedalus <redstonedaedalus@gmail.com>
2018-01-13 18:22:59 -0700

    Created stats command to show support stats.

---
+++ commands/information/stats.js
@@ -0,0 +1,30 @@
+const ms = require("pretty-ms");
+const Base = require("../../base/Command");
+const fetchSupportData = require("../../methods/restricted/fetchSupportData");
+
+module.exports = class Stats extends Base {
+    constructor(client) {
+        super(client, {
+            name: "stats",
+            description: "Check a support's stats.",
+            usage: "[user]",
+            permLevel: 2,
+            aliases: ["support"]
+        });
+    }
+
+    async run(message, args) {
+        // Fetch support data
+        const data = await fetchSupportData(this.client, message.author.id);
+        // If no data returned, throw an error
+        if (!data) return super.error(`No session data found.`);
+
+        message.channel.send([
+            "**__Support Statistics__**",
+            `**${data.sessions.length}** total sessions.`,
+            `**${Array.from(new Set(data.sessions.map(s => s.name))).length}** individual players helped.`,
+            `**${ms(data.sessions.reduce((t, s) => t += s.duration, 0), { verbose: true })}** of session time.`, //eslint-disable-line
+            `**${data.sessions.filter(s => new Date(new Date().setDate(new Date().getDate() - 30)) - s.time > 0).length}** sessions this month.`
+        ].join("\n"));
+    }
+};
