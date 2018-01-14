b7c76c89c64de158e77cb6f5ac746800cdeff1a3
RedstoneDaedalus <cosimoplumb@gmail.com>
2018-01-13 17:11:33 -0700

    Added functionality.

---
+++ commands/administrative/userinfo.js
@@ -0,0 +1,42 @@
+const { MessageEmbed } = require("discord.js");
+const Base = require("../../base/Command.js");
+
+class Userinfo extends Base {
+  constructor(client) {
+    super(client, {
+      name: "userinfo",
+      description: "Finds information on a user.",
+      usage: "<user>",
+      category: "administrative",
+      permLevel: 5
+    });
+  }
+  
+  async run(message, args) {
+    // Fetch the target user
+    const user = await super.verifyUser(args[0]);
+    const member = await message.guild.members.fetch(user.id);
+    // Confirm that a user has been found
+    if (!user) return super.error("Unknown user.");
+    
+    // Create a new embed
+    const embed = new MessageEmbed()
+      .setColor("#FFFFFF")
+      .setAuthor(user.tag)
+      .setFooter("VerifyBot User Information");
+     
+     // Add fields
+     embed.addField("» Name", user.username, true);
+     if (member.displayName !== user.username) embed.addField("» Nickname", member.displayName");
+     embed.addField("» Permission Level", await this.client.permLevel(user).name, true);
+     embed.addField("» Joined Discord", humanize(user.createdAt));
+     embed.addField("» Joined Server", humanize(member.joinedAt));
+  }
+  
+  // Used to make dates easy to read
+  humanize(date) {
+    // Define months
+    const months = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    return `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
+  }
+}
