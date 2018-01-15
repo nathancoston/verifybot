bcb6c28b38134e66c157187bc47e8696778ca8d4
RedstoneDaedalus <redstonedaedalus@gmail.com>
2018-01-14 17:51:46 -0700

    Added error catching.

--- commands/system/pull.js
+++ commands/system/pull.js
@@ -15,29 +15,33 @@ module.exports = class Pull extends Command {
     }
 
     async run(message) {
-        // Execute pull on repository URL
-        const { stdout, stderror, error } = await exec(`git pull ${require("../../package.json").repository.url.split("+")[1]}`, { cwd: path.join(__dirname, "../../") }); //eslint-disable-line global-require
-        // If error thrown, return error message
-        if (error) return super.error("An unknown error occured whilst attempting to pull.");
+        try {
+            // Execute pull on repository URL
+            const { stdout, stderror, error } = await exec(`git pull ${require("../../package.json").repository.url.split("+")[1]}`, { cwd: path.join(__dirname, "../../") }); //eslint-disable-line global-require
+            // If error thrown, return error message
+            if (error) return super.error("An unknown error occured whilst attempting to pull.");
 
-        // Create an empty array to store output.
-        const out = [];
-        // If output returned, push to output
-        if (stdout) out.push(stdout);
-        // If error returned, push to output
-        if (stderror) out.push(stderror);
+            // Create an empty array to store output.
+            const out = [];
+            // If output returned, push to output
+            if (stdout) out.push(stdout);
+            // If error returned, push to output
+            if (stderror) out.push(stderror);
 
-        await message.channel.send(out.join("---\n"), { code: true });
-        const notice = await message.channel.send("To update changes, you'll need to restart the bot. Respond with **y** to restart or **n** to cancel restart.");
-        
-        message.channel.awaitMessages(m => m.author.id === message.author.id && ["y", "n"].includes(m.content), { max: 1, time: 60000, errors: ["time"] })
-        .then(collected => {
-            const m = collected.first();
-            if (m.content === "y") return this.client.commands.get("restart").run(message);
-            notice.edit("To update changes, you'll need to restart the bot.");
-            super.respond("Restart canceled.");
-        }).catch(() => {
-            notice.edit("To update changes, you'll need to restart the bot.");
-        });
+            await message.channel.send(out.join("---\n"), { code: true });
+            const notice = await message.channel.send("To update changes, you'll need to restart the bot. Respond with **y** to restart or **n** to cancel restart.");
+            
+            message.channel.awaitMessages(m => m.author.id === message.author.id && ["y", "n"].includes(m.content), { max: 1, time: 60000, errors: ["time"] })
+            .then(collected => {
+                const m = collected.first();
+                if (m.content === "y") return this.client.commands.get("restart").run(message);
+                notice.edit("To update changes, you'll need to restart the bot.");
+                super.respond("Restart canceled.");
+            }).catch(() => {
+                notice.edit("To update changes, you'll need to restart the bot.");
+            });
+        } catch (e) {
+            super.error("An unknown error occured.");
+        }
     }
 };
