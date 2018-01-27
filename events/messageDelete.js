cb133a5dd5dd6f4afb6d53c3500910e5769569d8
RedstoneDaedalus <redstonedaedalus@gmail.com>
2018-01-26 19:35:31 -0700

    Now shows who deleted a message.

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -16,11 +16,16 @@ module.exports = class {
         // If message is a command or message is sent by a bot, ignore it
         if ((message.content.startsWith(config.prefix) && (this.client.commands.has(message.content.split(config.prefix)[1].split(" ")[0]) || this.client.aliases.has(message.content.split(config.prefix)[1].split(" ")[0]))) || message.author.bot) return;
 
+        // Attempt to find executor
+        const auditLogs = await message.guild.fetchAuditLogs({ limit: 1, type: "MESSAGE_DELETE" });
+        const entry = auditLogs.entries.first();
+        const executor = entry.executor.id === message.author.id ? null : entry.executor;
+
         // Create a new embed
         const embed = new MessageEmbed()
             .setColor([255, 100, 100])
             .setAuthor(`${message.member.displayName} (${message.author.tag})`)
-            .setTitle(`Message deleted in #${message.channel.name}.`)
+            .setTitle(`Message deleted in #${message.channel.name + executor === null ? "" : ` by ${executor.tag}`}.`)
             .setDescription(message.content)
             .setImage(message.attachments.size > 0 ? message.attachments.first().url : null)
             .setTimestamp();
