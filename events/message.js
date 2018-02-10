e652793984cee58a19475f585997ec4f0599daaf
RedstoneDaedalus <redstonedaedalus@gmail.com>
2018-02-09 19:54:48 -0700

    Spam filter

--- events/message.js
+++ events/message.js
@@ -1,16 +1,40 @@
 const levels = require("../levels.json");
 
+const recentMessages = new Map();
+
 module.exports = class {
     constructor(client) {
         this.client = client;
     }
 
     async run(message) {
-        // Ignore if sender is bot, there is no prefix, or if guild is invalid
-        if (message.author.bot || !message.guild || this.client.config.guild !== message.guild.id || message.content.indexOf(this.client.config.prefix) === -1) return;
+        // Ignore if sender is bot, or if guild is invalid
+        if (message.author.bot || !message.guild || this.client.config.guild !== message.guild.id) return;
 
         // Calculate permissions
         const userPerms = await this.client.permLevel(message.author.id);
+
+        // Repitition filter
+        let recent = recentMessages.get(message.author.id) || [];
+        if (recent[0]) {
+            if (recent[0] !== message.content.toLowerCase()) {
+                recentMessages.delete(message.author.id);
+                recent = [];
+            } else {
+                recent.push(message.content.toLowerCase());
+                recentMessages.set(message.author.id, recent);
+            }
+        } else recentMessages.set(message.author.id, [message.content.toLowerCase()]);
+        
+        if (recent.length > 4) {
+            const messages = await message.channel.messages.fetch({ limit: 10 });
+            const filtered = messages.filter(m => m.content.toLowerCase() === recent[0]);
+
+            message.channel.bulkDelete(filtered, true).then(() => message.channel.send(`${message.author} | ❌ | Please stop spamming. Your messages have been removed.`)).catch(() => null).then(m => m.delete({ timeout: 10000 }));
+        }
+
+        // Verify that message is a command
+        if (message.content.indexOf(this.client.config.prefix) === -1) return;
 
         // Fetch command name and arguments
         const args = message.content.split(/\s+/g);
