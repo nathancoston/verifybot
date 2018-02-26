e7d6d595a42303da0ebef2f739c3dca1d8bc3359
RedstoneDaedalus <redstonedaedalus@gmail.com>
2018-02-26 16:27:05 -0700

    Added comments to perms.js

--- commands/administrative/perms.js
+++ commands/administrative/perms.js
@@ -22,13 +22,18 @@ module.exports = class Perms extends Base {
         const permission = (args[2] || "").toLowerCase();
         const channels = message.mentions.channels;
 
+        // Validate arguments
         if (!["list", "give", "take", "reset"].includes(mode)) return super.error(`Command usage: \`${this.client.config.prefix}${this.help.name} ${this.help.usage}\`.`);
+        
+        // If mode is list, list all permissions
         if (mode === "list") return message.channel.send(`== All user permissions\n\n${flags.join("\n")}`, { code: "asciidoc" });
 
+        // Check if the flags, channels, and user are valid
         if (!flags.includes(permission)) return super.error("Invalid permission name.");
         if (channels.size === 0) return super.error("Please provide at least 1 channel.");
         if (!user) return super.error("No user found.");
 
+        // If mode is give, give the target user the specified permissions
         if (mode === "give") {
             channels.forEach(channel => {
                 channel.overwritePermissions(user, {
@@ -39,6 +44,7 @@ module.exports = class Perms extends Base {
             return super.respond(`Given permissions to ${user.tag}.`);
         }
 
+        // If mode is reset, reset the permissions of the specified user
         if (mode === "reset") {
             channels.forEach(channel => {
                 channel.overwritePermissions(user, {
@@ -49,6 +55,7 @@ module.exports = class Perms extends Base {
             return super.respond(`Reset permissions for ${user.tag}.`);
         }
 
+        // If mode is take, take the permissions from the target user
         if (mode === "take") {
             channels.forEach(channel => {
                 channel.overwritePermissions(user, {
