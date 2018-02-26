c78bad3637c26579e1d8f1fb4b1425b08bb51e4a
RedstoneDaedalus <redstonedaedalus@gmail.com>
2018-02-25 18:07:06 -0700

    Made verification use the client.query method

--- dashboard/routes/index.js
+++ dashboard/routes/index.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 
 const { MessageEmbed } = require("discord.js");
 const passport = require("passport");
+const ms = require("pretty-ms");
 const uuid = require("uuid/v4");
 
 const checkAuth = require("../functions/checkAuth");
@@ -77,7 +78,7 @@ router.get("/", checkAuth, async (req, res) => {
             client.connection.query(`UPDATE linked_accounts SET discord_id = ${req.user.id}, secret_key = null WHERE player_name = '${accInfo.data.player_name.replace(/[^a-z_\d]/ig)}'`);
 
             // Find the verification logs channel
-            const channel = member.guild.channels.find("name", client.config.logs.verification);
+            const channel = member.guild.channels.find("name", client.config.channels.verification);
             // If no channel found, return
             if (!channel) return;
             // Create a new embed
@@ -119,8 +120,8 @@ router.post("/", checkAuth, async (req, res) => {
     if (!member) return res.redirect("?error=You are not on the DiamondFire discord server!");
     // If user is verified, throw error
     if (perms.level >= 1) return res.redirect(`?error=You are already verified!`);
-    // If user does not fill out both forums, throw error
-    if (!req.body.username || !req.body.key) return res.redirect("?error=You need to fill out both your Minecraft username and key.");
+    // If user does not fill out the input, throw error
+    if (!req.body.key) return res.redirect("?error=Please provide a secret key.");
 
     // Fetch user attempts
     let attempts = client.attempts.get(req.user.id) || 0;
@@ -128,70 +129,69 @@ router.post("/", checkAuth, async (req, res) => {
     let cooldown = client.cooldowns.get(req.user.id) || Date.now();
 
     // If user is on cooldown, throw an error
-    if (cooldown > Date.now()) return res.redirect("?error=You are on cooldown. Please try again later.");
+    if (cooldown > Date.now()) return res.redirect(`?error=You are on cooldown. Please try again in ${ms(cooldown - Date.now(), { verbose: true })}.`);
 
     // Fetch verification information from linked accounts, sanitizing secret key
-    client.connection.query(`SELECT player_name, secret_key FROM linked_accounts WHERE secret_key = '${req.body.key.replace(/[^a-z\d]/ig, "")}'`, async (err, fields) => {
-        // If no data is returned, or player name specified does not match player name associated with input key...
-        if (!fields[0] || fields[0].player_name !== req.body.username) {
-            // Increment attempts
-            attempts++;
+    const fields = await client.query(`SELECT player_name, secret_key FROM linked_accounts WHERE secret_key = '${req.body.key.replace(/[^a-z\d]/ig, "")}'`);
+    // If no data is returned, or player name specified does not match player name associated with input key...
+    if (!fields[0]) {
+        // Increment attempts
+        attempts++;
 
-            // If attempts is greater than 1, set cooldown to one minute
-            if (attempts >= 1) cooldown += 60000;
-            // If attempts is greater than 7, set cooldown to one hour
-            if (attempts >= 7) cooldown += 600000;
+        // If attempts is greater than 1, set cooldown to one minute
+        if (attempts >= 1) cooldown += 60000;
+        // If attempts is greater than 7, set cooldown to one hour
+        if (attempts >= 7) cooldown += 600000;
 
-            // If attempts is greater than 10...
-            if (attempts >= 10) {
-                // Delete their attempt data
-                client.attempts.delete(req.user.id);
-                // Delete their cooldown data
-                client.cooldowns.delete(req.user.id);
-                // Ban them from the Discord server
-                member.ban({ reason: "10 attempts at verification." }).catch(() => null);
-                // Throw an error
-                return res.redirect("?error=Your key is invalid. Since you have over 10 attempts, you have been banned from the discord server.");
-            }
-
-            //Update attempts
-            client.attempts.set(req.user.id, attempts);
-            // Update cooldown
-            client.cooldowns.set(req.user.id, cooldown);
-
-            // Notify user that their key is invalid
-            return res.redirect(`?error=Your key is invalid. Please wait for ${attempts >= 7 ? "10 minutes" : "1 minute"} before verifying yourself again.`);
+        // If attempts is greater than 10...
+        if (attempts >= 10) {
+            // Delete their attempt data
+            client.attempts.delete(req.user.id);
+            // Delete their cooldown data
+            client.cooldowns.delete(req.user.id);
+            // Ban them from the Discord server
+            member.ban({ reason: "10 attempts at verification." }).catch(() => null);
+            // Throw an error
+            return res.redirect("?error=Your key is invalid. Since you have over 10 attempts, you have been banned from the discord server.");
         }
 
-        // Delete attempt data (if present)
-        client.attempts.delete(req.user.id);
-        // Delete cooldown data (if present)
-        client.cooldowns.delete(req.user.id);
+        //Update attempts
+        client.attempts.set(req.user.id, attempts);
+        // Update cooldown
+        client.cooldowns.set(req.user.id, cooldown);
 
-        // Generate a verification token
-        const token = uuid();
-        // Insert into the tokens collection the user's id, account info, and token
-        client.tokens.set(req.user.id, { data: fields[0], token });
-        // Generate a timestamp (used for embed footer)
-        const timestamp = new Date();
+        // Notify user that their key is invalid
+        return res.redirect(`?error=Your key is invalid. Please wait for ${attempts >= 7 ? "10 minutes" : "1 minute"} before verifying yourself again.`);
+    }
 
-        // Notify the user to check their direct messages
-        res.redirect("?message=Check your private messages on discord.");
-        // Send the user an embed with a link to verification confirmation
-        member.user.send({ embed: {
-            color: 3060589,
-            author: {
-                name: "Confirm Verification"
-            },
-            description: `Click [here](${client.config.dashboard.domain}?token=${token}) to verify your account. This will expire in **3** minutes.`,
-            timestamp
-        } }).catch(() => res.redirect("?error=You do not have direct messages enabled."));
+    // Delete attempt data (if present)
+    client.attempts.delete(req.user.id);
+    // Delete cooldown data (if present)
+    client.cooldowns.delete(req.user.id);
 
-        // In 3 minutes, delete their token information as it has expired
-        setTimeout(() => {
-            client.tokens.delete(req.user.id);
-        }, 180000);
-    });
+    // Generate a verification token
+    const token = uuid();
+    // Insert into the tokens collection the user's id, account info, and token
+    client.tokens.set(req.user.id, { data: fields[0], token });
+    // Generate a timestamp (used for embed footer)
+    const timestamp = new Date();
+
+    // Notify the user to check their direct messages
+    res.redirect("?message=Check your private messages on discord.");
+    // Send the user an embed with a link to verification confirmation
+    member.send({ embed: {
+        color: 3060589,
+        author: {
+            name: "Confirm Verification"
+        },
+        description: `Click [here](${client.config.dashboard.domain}?token=${token}) to verify your account. This will expire in **3** minutes.`,
+        timestamp
+    } }).catch(() => res.redirect("?error=You do not have direct messages enabled."));
+
+    // In 3 minutes, delete their token information as it has expired
+    setTimeout(() => {
+        client.tokens.delete(req.user.id);
+    }, 180000);
 });
 
 // Export the router
