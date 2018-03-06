3dd69220fa440373ec86bf44f7b49848d8dca552
RedstoneDaedalus <redstonedaedalus@gmail.com>
2018-03-06 09:21:48 -0700

    Bugfixes

--- commands/system/sql.js
+++ commands/system/sql.js
@@ -13,12 +13,17 @@ module.exports = class Sql extends Base {
     }
 
     async run(message, args) {
+        // Query the specified sql code
         this.client.query(args.join(" ")).then(results => {
+            // Send the list of results
             message.channel.send(`== ${args.join(" ")}\n\n${results.map(r => {
+                // Calculate spacing
                 const spacing = Object.keys(r).reduce((out, key) => Math.max(out, key.length), 0);
+                // Map all entries
                 return Object.entries(r).map(data => `${data[0] + " ".repeat(spacing - data[0].length)} :: ${data[1]}`).join("\n");
             }).join("\n\n")}`, { code: "asciidoc" });
         }).catch(err => {
+            // T
             message.channel.send(`SQL Error\n\`\`\`${err}\`\`\``);
         });
     }
